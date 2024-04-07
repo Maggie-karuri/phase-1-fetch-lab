1346ac13e9567497d3775e888f1a714f92336367
Maggie-karuri <karurimargaret111@gmail.com>
2024-04-07 12:44:06 +0300

    use of fetch request

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(books => {
+      // Render the books on the webpage
+      renderBooks(books);
+    })
+    .catch(error => {
+      console.error('There was a problem fetching the books:', error);
+    });
 }
 
 function renderBooks(books) {
